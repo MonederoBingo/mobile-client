782294f4dced76a40fa1dcf7edbe19cf85231baf
alayor <alayor3@gmail.com>
2017-10-23 22:55:58 -0500

    Pull data to show in detail view

--- PushPayload.js
+++ PushPayload.js
@@ -19,9 +19,20 @@ export default class PushPayload extends Component < {} > {
     });
     this.state = {
       dataSource: ds,
+      pushEvent: props.pushEvent
     }
   }
+  renderRow(rowData) {
+      return (
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
 
+        }}>
+          <Text>{rowData.sha.substring(0, 6)} - {rowData.message}</Text>
+        </View>
+      );
+  }
   render() {
     return (
       <View style={{
@@ -30,7 +41,29 @@ export default class PushPayload extends Component < {} > {
         justifyContent: 'flex-start',
         alignItems: 'center'
       }}>
-        <Text>Hello there</Text>
+        <Image
+          source={{uri: this.state.pushEvent.actor.avatar_url}}
+          style={{
+            height: 120,
+            width: 120,
+            borderRadius: 60,
+          }}
+        />
+        <Text style={{
+          paddingTop: 20,
+          paddingBottom: 20,
+          fontSize: 20,
+        }}>
+          {moment(this.state.pushEvent.created_at).fromNow()}
+        </Text>
+        <Text>{this.state.pushEvent.actor.login}</Text>
+        <Text>{this.state.pushEvent.payload.ref.replace('refs/heads/', '')}</Text>
+        <Text>at {this.state.pushEvent.repo.name}</Text>
+        <Text>{this.state.pushEvent.payload.commits.length} Commit(s)</Text>
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)} />
       </View>
     );
   }
